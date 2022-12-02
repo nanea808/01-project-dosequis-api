9034c54b6c408550fcacfeaacebb8796138d7ee9
Ethan Willers <willers.enw@gmail.com>
2022-12-01 20:37:14 -0800

    ticket master api call

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# 01-project-dosequis
+# 01-project-dosequis 
+
+## Description
+
+This webpage was created for our bootcamp's first project. 
+
+User Story
+
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+<!-- To use this Prework Study Guide, you can review the notes in each section. For suggestions on what to study first, open the Chrome DevTools by pressing Command+Option+I (macOS) or Control+Shift+I (Windows). A console panel should open either below or to the side of the webpage in the browser. There you will see a list of topics we learned from the prework along with a suggestion on which topic to study first. -->
+
+## Credits
+
+
+## Developers
+
+Snail Team 6
+
+Willers, Ethan https://github.com/nanea808 <br>
+Kok-Carlson, Jack https://github.com/BaldurTheBuilder <br>
+Pineda, Sharmaine https://github.com/tropical9 <br>
+Clark, Ty <br>
+Nelson, John https://github.com/fjnelson 
+
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,38 +6,51 @@ $(() => {
     // ## Function to load weather data as a modal element ♥ ##
 
     // ## Two Ticketmaster api functions ##
-        // # Event discovery api function ✈ #
-        /* 
-        Take user input and inject into api call
-        var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword=' + userInput + '&countryCode=US&apikey={apikey}'
+    var eventDiscovery = function (userInput) {
+        var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword=' + userInput + '&countryCode=US&apikey=iQvDtAeqOGfetg1ilGAAF6sw3ekPWih6'
+        fetch(requestUrl).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            } else {
+                // Replace alert with print to site (modal?)
+                alert('Error: ' + response.statusText);
+            }
+        })
+    }
+    eventDiscovery("underwood");
+    // # Event discovery api function ✈ #
+    /* 
+    Take user input and inject into api call
+    var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword=' + userInput + '&countryCode=US&apikey={apikey}'
 
-        Fetch request using requestUrl
+    Fetch request using requestUrl
 
-        Pass data into the function to load responses onto page ★
-        */
+    Pass data into the function to load responses onto page ★
+    */
 
-        // # Event location api function ♣ #
-        /* 
-        Take event id and inject into api call
-        var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events/' + id + '&apikey={apikey}'
+    // # Event location api function ♣ #
+    /* 
+    Take event id and inject into api call
+    var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events/' + id + '&apikey={apikey}'
 
-        Fetch request using requestUrl
+    Fetch request using requestUrl
 
-        Pass lat and lon data into the weather api function ☁
-        */
+    Pass lat and lon data into the weather api function ☁
+    */
 
     // ## One weather api function ☁ ##
-        /*
-        Take lat and lon and inject into api call
+    /*
+    Take lat and lon and inject into api call
 
-        Fetch request
+    Fetch request
 
-        Take reponse and pass into modal function ♥
-        */
+    Take reponse and pass into modal function ♥
+    */
 
     // ## Event listener to take user input and pass to Ticketmaster event discovery api function ✈ ##
 
     // ## Event listener to take user input on button elements ##
     /* Take event id from button elements and pass into location api function ♣ */
-    
 });
